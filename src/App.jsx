3430610661662514284d624a3b56d9dc26851bd9
AhaD <abuahad35188@gmail.com>
2024-04-03 14:15:14 +0600

    page set up and layout design

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+ 
+import './App.css'
+import LeftSide from './Components/LeftSide/LeftSide'
+import RightSide from './Components/RightSide/RightSide'
+
+function App() {
+ 
+
+  return (
+    <div className='flex gap-5 px-5'>
+       <div className='w-[180px] '>
+          <LeftSide></LeftSide>
+       </div>
+       <div className='b w-full'>
+          <RightSide></RightSide>
+       </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/LeftSide/LeftSide.jsx
@@ -0,0 +1,53 @@
+export default function LeftSide() {
+  return (
+    <div className="bg-[#fff] h-[598px] border-r border-[#F0F0F0]">
+      <a className=" " href="#">
+        <img
+          className="w-[152px] h-[48px]"
+          src="https://s3-alpha-sig.figma.com/img/49bc/c711/4fce897d1ad18e98913aa5ae12ca542c?Expires=1713139200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=UUC~hKDkgiTd~sNreiEjP1Ong93UdMeQ8pu7ZSNl8jdlQjNMQT8rn2o7VXmnSwEt65Xl0poeT5ZcVMAzZ0B5UfNyWGuiZavcRbgD7aLbZVfxyzeGTI4f2chJU4dDtmo4KM~N5-g9ZYIvamg2RdFhM~CGfJYwv9nPtIkPdWDmAwU5ITQ0Tu69EimJKMwktYeP2IIAP3AFPs2Off-mRDbWfgq~2QuBVdkuEKDX-nCDwhVhbCmnVnX~rg6I5dn3bEIE~dM9Zpl2xb-CXnir13f4tNuBRzs2~ENoU0v9wV1oEDkrxVF1iRDcU3YXlV1xuSvGwGcpu7yqMBVSf5AOKWa5jw__"
+          alt="logo"
+        />
+      </a>
+      {/* category */}
+
+      <ul className="mt-[15px]  b">
+        <li className=" px-[10px] hover:bg-[#F0F0F0] text-[#989898] hover:text-[#111111] my-3">
+          <a className="flex items-center gap-3 hover:text-[#111111] " href="#">
+          <span> <i className="my-icon icon-discover h-[20px] w-[20px] "></i></span>
+            <span className="font-[400] text-[14px] leading-[18px]  ">Discover</span>
+          </a>
+        </li>
+        <li className=" px-[10px] hover:bg-[#F0F0F0] text-[#989898] hover:text-[#111111] my-3">
+          <a className="flex items-center gap-3 hover:text-[#111111] " href="#">
+          <span> <i className="my-icon icon-ui h-[20px] w-[20px] "></i></span>
+            <span className="font-[400] text-[14px] leading-[18px]  ">Ui Kits</span>
+          </a>
+        </li>
+        <li className=" px-[10px] hover:bg-[#F0F0F0] text-[#989898] hover:text-[#111111] my-3">
+          <a className="flex items-center gap-3 hover:text-[#111111] " href="#">
+          <span> <i className="my-icon icon-screen h-[20px] w-[20px] "></i></span>
+            <span className="font-[400] text-[14px] leading-[18px]  ">Screens</span>
+          </a>
+        </li>
+        <li className=" px-[10px] hover:bg-[#F0F0F0] text-[#989898] hover:text-[#111111] my-3">
+          <a className="flex items-center gap-3 hover:text-[#111111] " href="#">
+          <span> <i className="my-icon icon-articale h-[20px] w-[20px] "></i></span>
+            <span className="font-[400] text-[14px] leading-[18px]  ">Articles</span>
+          </a>
+        </li>
+        <li className=" px-[10px] hover:bg-[#F0F0F0] text-[#989898] hover:text-[#111111] my-3">
+          <a className="flex items-center gap-3 hover:text-[#111111] " href="#">
+          <span> <i className="my-icon icon-sponser h-[20px] w-[20px] "></i></span>
+            <span className="font-[400] text-[14px] leading-[18px]  ">Become Sponsor</span>
+          </a>
+        </li>
+        <li className=" px-[10px] hover:bg-[#F0F0F0] text-[#989898] hover:text-[#111111] my-3">
+          <a className="flex items-center gap-3 hover:text-[#111111] " href="#">
+          <span> <i className="my-icon icon-hire h-[20px] w-[20px] "></i></span>
+            <span className="font-[400] text-[14px] leading-[18px]  ">Hire Flutter Dev</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
