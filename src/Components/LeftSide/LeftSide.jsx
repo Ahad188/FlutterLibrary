58eb4a5c44b5cb5f471d0878c523a4b5bf512338
AhaD <abuahad35188@gmail.com>
2024-04-03 14:31:03 +0600

    left side menu done

--- src/Components/LeftSide/LeftSide.jsx
+++ src/Components/LeftSide/LeftSide.jsx
@@ -10,42 +10,90 @@ export default function LeftSide() {
       </a>
       {/* category */}
 
-      <ul className="mt-[15px]  b">
+      <ul className="mt-[15px]  ">
         <li className=" px-[10px] hover:bg-[#F0F0F0] text-[#989898] hover:text-[#111111] my-3">
           <a className="flex items-center gap-3 hover:text-[#111111] " href="#">
-          <span> <i className="my-icon icon-discover h-[20px] w-[20px] "></i></span>
+            <span>
+              {" "}
+              <i className="my-icon icon-discover h-[20px] w-[20px] "></i>
+            </span>
             <span className="font-[400] text-[14px] leading-[18px]  ">Discover</span>
           </a>
         </li>
         <li className=" px-[10px] hover:bg-[#F0F0F0] text-[#989898] hover:text-[#111111] my-3">
           <a className="flex items-center gap-3 hover:text-[#111111] " href="#">
-          <span> <i className="my-icon icon-ui h-[20px] w-[20px] "></i></span>
+            <span>
+              {" "}
+              <i className="my-icon icon-ui h-[20px] w-[20px] "></i>
+            </span>
             <span className="font-[400] text-[14px] leading-[18px]  ">Ui Kits</span>
           </a>
         </li>
         <li className=" px-[10px] hover:bg-[#F0F0F0] text-[#989898] hover:text-[#111111] my-3">
           <a className="flex items-center gap-3 hover:text-[#111111] " href="#">
-          <span> <i className="my-icon icon-screen h-[20px] w-[20px] "></i></span>
+            <span>
+              {" "}
+              <i className="my-icon icon-screen h-[20px] w-[20px] "></i>
+            </span>
             <span className="font-[400] text-[14px] leading-[18px]  ">Screens</span>
           </a>
         </li>
         <li className=" px-[10px] hover:bg-[#F0F0F0] text-[#989898] hover:text-[#111111] my-3">
           <a className="flex items-center gap-3 hover:text-[#111111] " href="#">
-          <span> <i className="my-icon icon-articale h-[20px] w-[20px] "></i></span>
+            <span>
+              {" "}
+              <i className="my-icon icon-articale h-[20px] w-[20px] "></i>
+            </span>
             <span className="font-[400] text-[14px] leading-[18px]  ">Articles</span>
           </a>
         </li>
         <li className=" px-[10px] hover:bg-[#F0F0F0] text-[#989898] hover:text-[#111111] my-3">
           <a className="flex items-center gap-3 hover:text-[#111111] " href="#">
-          <span> <i className="my-icon icon-sponser h-[20px] w-[20px] "></i></span>
+            <span>
+              {" "}
+              <i className="my-icon icon-sponser h-[20px] w-[20px] "></i>
+            </span>
             <span className="font-[400] text-[14px] leading-[18px]  ">Become Sponsor</span>
           </a>
         </li>
         <li className=" px-[10px] hover:bg-[#F0F0F0] text-[#989898] hover:text-[#111111] my-3">
           <a className="flex items-center gap-3 hover:text-[#111111] " href="#">
-          <span> <i className="my-icon icon-hire h-[20px] w-[20px] "></i></span>
+            <span>
+              {" "}
+              <i className="my-icon icon-hire h-[20px] w-[20px] "></i>
+            </span>
             <span className="font-[400] text-[14px] leading-[18px]  ">Hire Flutter Dev</span>
           </a>
+        </li>
+      </ul>
+      {/* 2nd category */}
+      <ul className="mt-[20px] px-[10px]">
+        <li className="font-[400] text-[14px] leading-[18px] text-[#111111] w-[148px] h-[20px]">
+          All Screens
+        </li>
+        <li className="my-3 flex justify-between text-[#989898] hover:text-[#111111]">
+          <span className=" text-[14px] leading-[18px] font-[400]">Sing In</span>
+          <span className=" text-[14px] leading-[18px] font-[400]">10</span>
+        </li>
+        <li className="my-3 flex justify-between text-[#989898] hover:text-[#111111]">
+          <span className=" text-[14px] leading-[18px] font-[400]">Chat</span>
+          <span className=" text-[14px] leading-[18px] font-[400]">5</span>
+        </li>
+        <li className="my-3 flex justify-between text-[#989898] hover:text-[#111111]">
+          <span className=" text-[14px] leading-[18px] font-[400]">Onboarding</span>
+          <span className=" text-[14px] leading-[18px] font-[400]">18</span>
+        </li>
+        <li className="my-3 flex justify-between text-[#989898] hover:text-[#111111]">
+          <span className=" text-[14px] leading-[18px] font-[400]">E-commerce</span>
+          <span className=" text-[14px] leading-[18px] font-[400]">9</span>
+        </li>
+        <li className="my-3 flex justify-between text-[#989898] hover:text-[#111111]">
+          <span className=" text-[14px] leading-[18px] font-[400]">Checkout</span>
+          <span className=" text-[14px] leading-[18px] font-[400]">10</span>
+        </li>
+        <li className="my-3 flex justify-between text-[#989898] hover:text-[#111111]">
+          <span className=" text-[14px] leading-[18px] font-[400]">Error</span>
+          <span className=" text-[14px] leading-[18px] font-[400]">16</span>
         </li>
       </ul>
     </div>
